882474d3bfcb04a8c9040e97498122ac8f7375a9
Chris Sanders <astericky@gmail.com>
2018-08-09 21:32:15 -0400

    first commit

---
+++ Sources/App/routes.swift
@@ -0,0 +1,11 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+  router.post("api", "acronyms") { req -> Future<Acronym> in
+    return try req.content.decode(Acronym.self).flatMap(to: Acronym.self, { acronym in
+      return acronym.save(on: req)
+    })
+  }
+  
+}
